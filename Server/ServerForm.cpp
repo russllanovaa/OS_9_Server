198b4035c03c280c1ef78c85e26b54ed4fa1ca9f
russllanovaa <pobirska.ksenia@gmail.com>
2024-11-19 23:30:14 +0200

    add labels

--- Server/ServerForm.cpp
+++ Server/ServerForm.cpp
@@ -2,8 +2,7 @@
 
 static bool moderate = true;
 
-System::Void Server::ServerForm::button1_Click(System::Object^ sender, System::EventArgs^ e)
-{
+System::Void Server::ServerForm::button1_Click(System::Object^ sender, System::EventArgs^ e) {
     if (moderate == false)
     {
         label5->Text = "ON";
@@ -18,14 +17,11 @@ System::Void Server::ServerForm::button1_Click(System::Object^ sender, System::E
     }
 
 }
-}
 
-System::Void Server::ServerForm::button2_Click(System::Object^ sender, System::EventArgs^ e)
-{
+System::Void Server::ServerForm::button2_Click(System::Object^ sender, System::EventArgs^ e) {
     return System::Void();
 }
 
-System::Void Server::ServerForm::button3_Click(System::Object^ sender, System::EventArgs^ e)
-{
+System::Void Server::ServerForm::button3_Click(System::Object^ sender, System::EventArgs^ e) {
     return System::Void();
 }
--- Server/ServerForm.h
+++ Server/ServerForm.h
@@ -42,6 +42,8 @@ namespace Server {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
 
 	private:
 		/// <summary>
@@ -63,6 +65,8 @@ namespace Server {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -111,6 +115,7 @@ namespace Server {
 			this->button1->TabIndex = 4;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &ServerForm::button1_Click);
 			// 
 			// button2
 			// 
@@ -120,6 +125,7 @@ namespace Server {
 			this->button2->TabIndex = 5;
 			this->button2->Text = L"button2";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &ServerForm::button2_Click);
 			// 
 			// button3
 			// 
@@ -129,12 +135,33 @@ namespace Server {
 			this->button3->TabIndex = 6;
 			this->button3->Text = L"button3";
 			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &ServerForm::button3_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(736, 47);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(44, 16);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"label4";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(749, 181);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(44, 16);
+			this->label5->TabIndex = 8;
+			this->label5->Text = L"label5";
 			// 
 			// ServerForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(931, 556);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
@@ -151,5 +178,8 @@ namespace Server {
 		}
 #pragma endregion
 	
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e);
 };
 }
